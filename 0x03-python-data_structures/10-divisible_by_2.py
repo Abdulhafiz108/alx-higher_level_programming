ef8455cba5e097c53cdeec88f6b7b76b1c55398a
Abdulhafiz Gazali Ado <Ahgazali108@gmail.com>
2023-06-12 19:39:01 +0000

    python-data_structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,6 +5,6 @@ def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             cpy.append(True)
-        else
+        else:
             cpy.append(False)
     return cpy
