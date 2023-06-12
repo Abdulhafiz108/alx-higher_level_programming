d1b470c0b31987819b370b591b4ef362a4751f9d
Abdulhafiz Gazali Ado <Ahgazali108@gmail.com>
2023-06-12 17:34:56 +0000

    python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,6 @@
 def new_in_list(my_list, idx, element):
 
     cpy = my_list.copy()
-    if idx < 0 and idx > (len(my_list) - 1):
-        return my_list
-    cpy[idx] = element
+    if idx > 0 and idx < len(my_list):
+        cpy[idx] = element
     return cpy
