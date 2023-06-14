868adbb76e16d07da5c30f6253f943c05380ae0a
Abdulhafiz108 <Ahgazali108@gmail.com>
2023-06-14 22:21:23 +0000

    more_data_structures

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+
+    new = list(a_dictionary.keys())
+    for i in new:
+        if a_dictionary.get(i) == value:
+            del a_dictionary[i]
+    return a_dictionary
