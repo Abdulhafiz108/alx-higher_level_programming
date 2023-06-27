95f74a514cb3fdce1ea4bcaea23e206499f82c20
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-06-27 23:14:01 +0000

    python-classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -2,7 +2,7 @@
 """Define a class Square."""
 
 
-class Size:
+class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
@@ -24,7 +24,8 @@ class Size:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """Return the current area of the square."""
-        return (self.__size ** 2)
+        return (self.__size * self.__size)
