b3598c58dab20eb2c1abd862cfe6504d707290a5
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-11 22:36:33 +0000

    javascript-warm_up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
-const count = process.argv.length - 2;
+let count = process.argv.length - 2;
 
-if (count === 0)
-{
+if (count === 0) {
 	console.log('No argument');
 }
-else if (count === 1)
-{
+else if (count === 1) {
 	console.log('Argument found');
 }
-else
-{
+else {
 	console.log('Arguments found');
 }
