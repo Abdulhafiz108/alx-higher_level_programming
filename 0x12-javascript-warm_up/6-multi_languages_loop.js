bfec8b1620161ec49b04fc5248de8543e5fb2825
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-11 23:56:05 +0000

    javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 let list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(list[0] + '\n' + list[1] + '\n' + list[2]);
+for (let i = 0; i < 3; i++) {
+	console.log(list[i])
+}
