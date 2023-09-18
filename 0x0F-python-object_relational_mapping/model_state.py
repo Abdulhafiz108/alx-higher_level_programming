656d98cf465ad9550ba65d2a60779c99f8420f89
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-18 21:43:29 +0000

    object_relational_mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 """
 Module that connects to the database
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.delarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
