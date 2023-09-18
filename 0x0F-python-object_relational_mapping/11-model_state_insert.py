244af90b04dd78183aea2b899a9312257a5463db
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-18 23:43:17 +0000

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prints first State object from a database
+"""
+from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+    )
+
+    session_maker = sessionmaker(bind=engine)
+    my_session = session_maker()
+
+    tmp_obj = State(name="Louisiana")
+    my_session.add(tmp_obj)
+    my_session.commit()
+    print(tmp_obj.id)
+
+    my_session.close()
