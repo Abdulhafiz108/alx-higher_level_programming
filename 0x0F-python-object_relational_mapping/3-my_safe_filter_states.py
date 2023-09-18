5c30711f28a65bef8ad1313d55e948276604fb6f
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-18 15:56:06 +0000

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Displays all values where name matches argument
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    mydb = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+    )
+    mycur = mydb.cursor()
+    mycur.execute(
+            """
+            SELECT * FROM states WHERE name LIKE BINARY %s
+            ORDER BY states.id ASC;
+            """, (argv[4],)
+    )
+    my_data = mycur.fetchall()
+    for i in my_data:
+        print(i)
+    mycur.close()
+    mydb.close()
