f38cb32eb7912393276eddf6262f9102cbe19fdb
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-18 15:10:00 +0000

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Displays all values where name matches argument
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    mydb = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+    )
+    mycur = mydb.cursor()
+    mycur.execute(
+            "SELECT * FROM states "
+            "WHERE name LIKE BINARY '{}' ".format(argv[4])
+            "ORDER BY states.id ASC;"
+    )
+    my_data = mycur.fetchall()
+    for i in my_data:
+        print(i)
+    mycur.close()
+    mydb.close()
