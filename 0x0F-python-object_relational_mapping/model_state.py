cd038eee09e91e31a367d64dedf32473a3f3b737
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-18 21:11:46 +0000

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+link class to table in database
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = ceate_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
