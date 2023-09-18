f40c7c4225830a4d2630f933bbc9f3f2e2cf8c97
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-18 16:57:59 +0000

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Displays all values where name matches argument
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    mydb = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306
+    )
+    mycur = mydb.cursor()
+    mycur.execute(
+            """
+            SELECT * FROM cities
+            INNER JOIN states
+            ON cities.state_id = states.id
+            ORDER BY cities.id ASC;
+            """
+    )
+    my_data = mycur.fetchall()
+    j = 0
+    for i in my_data:
+        if i[4] == argv[4]:
+            if j != 0:
+                print(", ")
+            print(i[2])
+            j = 1
+    mycur.close()
+    mydb.close()
