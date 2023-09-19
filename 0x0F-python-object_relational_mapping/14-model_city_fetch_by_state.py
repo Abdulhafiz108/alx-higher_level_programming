8fe65299e2c66ccedbae29adefa2d2c436c7a75c
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-19 15:06:05 +0000

    object_relational_mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
     session_maker = sessionmaker(bind=engine)
     my_session = session_maker()
 
-    cities = my_session.query(City, State).filter(State.id == City.state_id).all()
+    cities = my_session.query(City, State).filter(
+            State.id == City.state_id).all()
     for state, city in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
