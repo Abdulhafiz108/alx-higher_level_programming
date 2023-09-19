81c608e909a430a8f4bcc123f6ee98bd11d9ebf3
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-19 15:02:46 +0000

    object_relational_mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,7 +5,7 @@ from sqlalchemy import (create_engine)
 import sys
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from model_city import city
+from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine(
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@
 """
 Module that connects to the database
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
@@ -16,4 +16,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
