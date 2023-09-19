adb25acaf4ee931fa809ed15dc6d207915f78ee6
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2023-09-19 00:02:38 +0000

    object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Deletes all State object with letter a in a database
+"""
+from sqlalchemy import (create_engine)
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+    )
+
+    session_maker = sessionmaker(bind=engine)
+    my_session = session_maker()
+
+    states = my_session.query(State).all()
+    for state in states:
+        if "a" in state.name:
+            my_session.delete(state)
+
+    my_session.commit()
+
+    my_session.close()
